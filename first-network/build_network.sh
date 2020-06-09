08505c0b8169b8884e9c1e2be2189f015726aa3e
weishancc <weishancc@gmail.com>
2020-06-09 11:34:24 +0800

    Modify .gitignore && remove scripts

--- first-network/build_network.sh
+++ first-network/build_network.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 function generateCerts() {
   if [ -d "crypto-config" ]; then
--- first-network/javascript/invoke.js
+++ first-network/javascript/invoke.js
@@ -38,7 +38,7 @@ async function main() {
         // Submit the specified transaction.
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
-        await contract.submitTransaction('save_data', 'meter0', '2020/6/4', 'A', '600', '001');
+        await contract.submitTransaction('save_data', 'meter0', '2020/6/4', 'N', '600', '001');
         console.log('Transaction has been submitted');
 
         // Disconnect from the gateway.
