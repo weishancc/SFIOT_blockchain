3c1dd6ea55d7d7f80c03bf9bd90eb6ffb3d99fe0
weishancc <weishancc@gmail.com>
2020-08-17 16:53:17 +0800

    Divide node3 && Bug fix

--- first-network/delete_network.sh
+++ first-network/delete_network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose -f docker-compose-cli.yaml -f docker-compose-etcdraft2.yaml -f docker-compose-couch.yaml -f docker-compose-org4.yaml -f docker-compose-ca.yaml down --volumes --remove-orphans
+docker-compose -f docker-compose-cli.yaml down --volumes --remove-orphans
 
 docker stop $(docker ps -aq)
 
